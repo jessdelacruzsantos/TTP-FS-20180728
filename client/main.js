5dc12433f9b368dd70d995d59b2953ac725ddd38
Jessie De La Cruz Santos <jessdelacruzsantos@gmail.com>
2018-12-02 19:45:33 -0800

    (/client/main): conditionally render based on login status

--- client/main.js
+++ client/main.js
@@ -1,4 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Switch, Route} from 'react-router-dom'
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('app'));
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+
+    render() {
+        if(this.state.isLoggedIn) {
+            return (<h1>Welcome back</h1>)
+        } else {
+            return (<h1>I don't know you</h1>)
+        }
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
